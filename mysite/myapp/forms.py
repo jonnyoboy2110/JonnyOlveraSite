dcfebadaf8059e3760216ca92e1212bdc1c1644f
Jonathan Olvera <jonnyoboy2110@yahoo.com>
2020-11-05 02:45:47 +0000

    setting up front end for type racer

--- mysite/myapp/forms.py
+++ mysite/myapp/forms.py
@@ -1,3 +1,16 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+
+class SuggestionForm(forms.Form):
+    paragraph = forms.CharField(
+        label='Paragraph',
+        required=True,
+        max_length=240,
+    )
+
+    def save(self, request):
+        paragraph_instance = models.paragraphModel()
+        paragraph_instance.paragraph = self.cleaned_data["paragraph"]
+        paragraph_instance.save()
+        return suggestion_instance
--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,2 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+# Create your models here.
+class ParagraphModel(models.Model):
+    Paragraph = models.CharField(max_length=240)
+
+    def __str__(self):
+        return self.suggestion
